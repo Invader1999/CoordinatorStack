ff6ab334782d8305394b2d3393de6119eb5ac5a0
Hemanth <hemanthreddy056@gmail.com>
2024-11-14 06:16:21 +0530

    Initial Commit

---
+++ CoordinatorStack/CoordinatorStackApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoordinatorStackApp.swift
+//  CoordinatorStack
+//
+//  Created by Kareddy Hemanth Reddy on 14/11/24.
+//
+
+import SwiftUI
+
+@main
+struct CoordinatorStackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
